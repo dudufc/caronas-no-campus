3fb0bee1e37575b3f24f7b5abd183537c10b044a
dudufc <2.11351602e+08+dudufc@users.noreply.github.com>
2026-06-29 12:57:23 +0000

    Fix: Garantido carregamento da rota raiz quando nenhum parâmetro é passado

--- public/index.php
+++ public/index.php
@@ -7,6 +7,11 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../app/Router.php';
 
+// Garantir que a URL padrão seja a raiz se nada for passado
+if (!isset($_GET['url'])) {
+    $_GET['url'] = '/';
+}
+
 // Inicializar e processar requisição
 $router = new Router();
 $router->processar();
